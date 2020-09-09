bfb034f8a5ed9681c890ab8479d7256c914d9c45
SSaeki-jp <es2dazbiz@gmail.com>
2020-09-09 18:49:05 +0900

    アクティブハッシュの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,28 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
 
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  def product_params
+    params.require(:product).permit(:image, :name, :explanation, :category_id, :product_status_id, :shipping_charges_id, \
+      :prefecture_id, :estimated_shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end

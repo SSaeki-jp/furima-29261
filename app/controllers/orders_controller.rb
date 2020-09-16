70103d811b41689f54452a3ba0ec4d521296a319
SSaeki-jp <es2dazbiz@gmail.com>
2020-09-16 16:30:34 +0900

    商品購入画面に商品情報が表示されるよう修正、JavaScriptでリクエストを送るためのコードを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,21 @@
 class OrdersController < ApplicationController
-  def index
-  end
+  before_action :move_to_index
 
-  def new
+  def index
+    @product = Product.find(params[:product_id])
   end
 
   def create
+    @order = Order.new(price: order_params[:price])
+  end
+
+  private
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def order_params
+    params.permit(:price, :token)
   end
 end

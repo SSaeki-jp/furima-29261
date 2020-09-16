62340074b505b31f85ac009f5cb22b60663ae867
SSaeki-jp <es2dazbiz@gmail.com>
2020-09-16 17:48:07 +0900

    マイグレーションファイルの修正

--- db/migrate/20200902033135_create_products.rb
+++ db/migrate/20200902033135_create_products.rb
@@ -1,15 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string  :name,                       null: false
-      t.text    :explanation,                null: false
-      t.integer :user_id,                    foreign_key: true
-      t.integer :category_id,                null: false
-      t.integer :product_status_id,          null: false
-      t.integer :shipping_charge_id,         null: false
-      t.integer :prefecture_id,              null: false
-      t.integer :estimated_shipping_date_id, null: false
-      t.integer :price,                      null: false
+      t.string     :name,                       null: false
+      t.text       :explanation,                null: false
+      t.references :user,                       foreign_key: true
+      t.integer    :category_id,                null: false
+      t.integer    :product_status_id,          null: false
+      t.integer    :shipping_charge_id,         null: false
+      t.integer    :prefecture_id,              null: false
+      t.integer    :estimated_shipping_date_id, null: false
+      t.integer    :price,                      null: false
       t.timestamps
     end
   end

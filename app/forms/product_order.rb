af24541d41c945d2926ff8411d9177e4605ff3b8
SSaeki-jp <es2dazbiz@gmail.com>
2020-09-21 18:24:28 +0900

    商品購入時の電話番号のバリデーションが誤っていたため修正

--- app/forms/product_order.rb
+++ app/forms/product_order.rb
@@ -8,8 +8,10 @@ class ProductOrder
   validates :postal_code, presence: true, format: { with: POSTAL_CODE_REGEX, message: 'は「-」を含む7桁で入力してください' }
   validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
   validates :city, :address, presence: true
-  PHONE_NUMBER_REGEX = /\A\d{11}\z/.freeze
+  PHONE_NUMBER_REGEX = /\A\d{10,11}\z/.freeze
   validates :phone_number, presence: true, format: { with: PHONE_NUMBER_REGEX, message: 'は半角数字の11桁以内で入力してください' }
+
+  # validates :phone_number, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 99_999_999_999 }
 
   def save
     order = Order.create(user_id: user_id, product_id: product_id)
